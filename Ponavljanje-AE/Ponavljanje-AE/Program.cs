f9ea583172073406d176851f2338848e4547fccf
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-10-17 23:35:50 +0200

    Add files via upload

---
+++ Ponavljanje-AE/Ponavljanje-AE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_AE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno Hrast = new Bjelogoricno(true);
+            Crnogoricno Bor = new Crnogoricno(true);
+            Console.WriteLine(Hrast.OpadajuListovi);
+            Console.WriteLine(Bor.OpadajuListovi);
+            Console.ReadKey();
+
+        }
+    }
+}
